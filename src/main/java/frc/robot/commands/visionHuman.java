be8d9ba807205f7845e17b3d40238fa9896aa169
gmnielsen <nielsen@cheverus.org>
2019-02-17 14:21:03 -0500

    fixing

--- src/main/java/frc/robot/commands/visionHuman.java
+++ src/main/java/frc/robot/commands/visionHuman.java
@@ -9,7 +9,7 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.command.Command;
-//import frc.robot.Robot;
+import frc.robot.Robot;
 
 public class visionHuman extends Command {
   public visionHuman() {
